0b6588f04abc6a3fde5578bb3be953e08ae77397
Craig Bennett <prefrontal@gmail.com>
2020-09-05 14:28:36 -0700

    Working #30, but hitting time limit

--- answers-python/0030-SubstringConcatenation.py
+++ answers-python/0030-SubstringConcatenation.py
@@ -9,37 +9,57 @@ import itertools
 from typing import List
 
 def findSubstring(s: str, words: List[str]) -> List[int]:
-
     if not s or not words:
         return []
 
+    results = []
+
     # Generate the substring permutations
     substring_combos = []
-    
-    for i in range(1, len(words)+1):
-        #els = [list(x) for x in itertools.combinations(words, i)]
-        els = [''.join(x) for x in itertools.permutations(words, i)]
-        substring_combos.extend(els)
+    substring_len = len(words[0]) * len(words)
 
-    print(substring_combos)
+    els = [''.join(x) for x in itertools.permutations(words, len(words))]
+    substring_combos.extend(els)
 
-    return[]
+    # Find the shortest and longest substring
+    max_substring_len = len(max(substring_combos, key=len))
+    min_substring_len = len(min(substring_combos, key=len))
 
+    substring_set = set(substring_combos)
 
+    # Search for the substrings
+    for i in range(0, len(s)):
+        string = s[i:(i+ substring_len)]
+        #print(string)
+        if string in substring_set:
+            longest_match = len(string)
+            results.append(i)
 
-
+    return results
 
 
 # Tests
 
+s = ""
+words = ["foo", "bar"]
+print(findSubstring(s, words) == [])
+
 s = "barfoothefoobarman"
-words = ["foo","bar"]
+words = []
+print(findSubstring(s, words) == [])
+
+s = "barfoothefoobarman"
+words = ["foo", "bar"]
 print(findSubstring(s, words) == [0,9])
 
 s = "wordgoodgoodgoodbestword"
-words = ["word","good","best","word"]
+words = ["word", "good", "best", "word"]
 print(findSubstring(s, words) == [])
 
 s = "barfoofoobarthefoobarman"
-words = ["bar","foo","the"]
+words = ["bar", "foo", "the"]
 print(findSubstring(s, words) == [6,9,12])
+
+s = "wordgoodgoodgoodbestword"
+words = ["word", "good", "best", "good"]
+print(findSubstring(s, words) == [8])
